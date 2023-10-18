0880c4450b7e9d4025d373311f391bb6165b7f3d
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-10-18 14:38:39 +0200

    feat: add class register

--- lib/register.dart
+++ lib/register.dart
@@ -5,6 +5,20 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
 
+class Register {
+  final String username;
+  final String email;
+  final String password;
+  final File? image;
+
+  Register({
+    required this.username,
+    required this.email,
+    required this.password,
+    required this.image,
+  });
+}
+
 class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
@@ -30,59 +44,40 @@ class _RegisterPageState extends State<RegisterPage> {
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
                     color: Color(0xFF2661FA),
-                    fontSize: 36
-                ),
+                    fontSize: 36),
                 textAlign: TextAlign.left,
               ),
             ),
-
             SizedBox(height: size.height * 0.03),
-
             Container(
               alignment: Alignment.center,
               margin: EdgeInsets.symmetric(horizontal: 40),
               child: TextField(
-                decoration: InputDecoration(
-                    labelText: "Username"
-                ),
+                decoration: InputDecoration(labelText: "Username"),
               ),
             ),
-
             SizedBox(height: size.height * 0.03),
-
             Container(
               alignment: Alignment.center,
               margin: EdgeInsets.symmetric(horizontal: 40),
               child: TextField(
-                decoration: InputDecoration(
-                    labelText: "Email"
-                ),
+                decoration: InputDecoration(labelText: "Email"),
               ),
             ),
-
             SizedBox(height: size.height * 0.03),
-
             Container(
               alignment: Alignment.center,
               margin: EdgeInsets.symmetric(horizontal: 40),
               child: TextField(
-                decoration: InputDecoration(
-                    labelText: "Password"
-                ),
+                decoration: InputDecoration(labelText: "Password"),
               ),
             ),
-
             SizedBox(height: size.height * 0.03),
-
             TextButton(
               child: Text('Select Image'),
               onPressed: _pickImageFromGallery,
             ),
-
-            _image == null
-                ? Text('No Image selected')
-                : Image.file(_image!),
-
+            _image == null ? Text('No Image selected') : Image.file(_image!),
             Container(
               alignment: Alignment.centerRight,
               margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
@@ -96,39 +91,33 @@ class _RegisterPageState extends State<RegisterPage> {
                   width: size.width * 0.5,
                   decoration: new BoxDecoration(
                       borderRadius: BorderRadius.circular(80.0),
-                      gradient: new LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 255, 136, 34),
-                            Color.fromARGB(255, 255, 177, 41)
-                          ]
-                      )
-                  ),
+                      gradient: new LinearGradient(colors: [
+                        Color.fromARGB(255, 255, 136, 34),
+                        Color.fromARGB(255, 255, 177, 41)
+                      ])),
                   padding: const EdgeInsets.all(0),
                   child: Text(
                     "SIGN UP",
                     textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
-                    ),
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
             ),
-
             Container(
               alignment: Alignment.centerRight,
               margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
               child: GestureDetector(
                 onTap: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()))
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()))
                 },
                 child: Text(
                   "Already Have an Account? Sign in",
                   style: TextStyle(
                       fontSize: 12,
                       fontWeight: FontWeight.bold,
-                      color: Color(0xFF2661FA)
-                  ),
+                      color: Color(0xFF2661FA)),
                 ),
               ),
             )
@@ -139,7 +128,8 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   Future _pickImageFromGallery() async {
-    final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
     if (pickedImage != null) {
       setState(() {
         _image = File(pickedImage.path);
